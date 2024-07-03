81e9ec1972cf257c106f26b256194d6fc536e1d5
Michael Young <127144770+Mcyoung20@users.noreply.github.com>
2024-07-03 08:32:22 -0700

    Update Retired.ps1

--- Retired.ps1
+++ Retired.ps1
@@ -8,12 +8,12 @@ $FileDate =  (Get-Date).toString("MM-dd-yyyy")
 # Create a yyyMMddHHmmss.Z formatted date string in UTC. Change the date before running this script.
 $RetireDate= ( Get-Date 'Monday, December 31, 2018 2:41:59 PM' ).ToUniversalTime().ToString('yyyMMddHHmmss.Z')
 
-#Imports list from Ninja. That list is created by exporting all workstations and uploading to the IT Share.
-$NinjaList = Import-Csv \\*SERVER*\IT\EOL\NinjaExport.csv
+#Imports list from Ninja. That list is created by exporting all workstations and uploading them to the IT Share.
+$NinjaList = Import-Csv \\*SERVER*\NinjaExport.csv
 
 #Runs through the list to find which computers are older than the retire date and puts them into a CSV.
 foreach ($Computer in $NinjaList){
-    #For some reason Ninja put a space in their object, so this line exists to make the space less annoying. Without it, the filter does not work.
+    #For some reason, Ninja put a space in their object, so this line exists to make the space less annoying. Without it, the filter does not work.
     $Name = $Computer."Display Name"
 
     #Checks the user attached to the computer.
@@ -22,10 +22,10 @@ foreach ($Computer in $NinjaList){
     $UN = $NinjaUser.split("\",2)[1]
     $User = Get-ADUser $UN
 
-    #If the users is not disabled, gets the computer object and adds it to to CSV. Otherwise, moves on to the next computer.
+    #If the user is not disabled, gets the computer object and adds it to to CSV. Otherwise, moves on to the next computer.
     if ("$User.Enabled" -ne "False") {
         Get-ADComputer -Filter {name -eq $Name -and whenCreated -lt $RetireDate} -Properties Name,OperatingSystem,Created,LastLogonDate,Description |
         Select-Object  Name,OperatingSystem,Created,LastLogonDate,Description |
-        Export-Csv -Path \\*SERVER*\it\EOL\EOLList_$FileDate.csv -Append
+        Export-Csv -Path \\*SERVER*\EOLList_$FileDate.csv -Append
     }
 }
